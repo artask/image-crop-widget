b1c12b463fe9500d9065d9895fd3ff72d6aca1ea
root <root@premiummen.com>
2017-07-09 01:00:33 +0000

    cleanup

--- image_crop_widget_autoupload.js
+++ image_crop_widget_autoupload.js
@@ -8,7 +8,7 @@
         //$('.form-item .form-submit[value=Upload]', context).hide();
         $('.form-item .form-file', context).change(function() {
             $parent = $(this).closest('.form-item');
-            console.log($('.form-submit[value=Upload]', $parent));
+            //console.log($('.form-submit[value=Upload]', $parent));
             $('.form-submit[value=Upload]', $parent).mousedown();
 
         });
